4e791d9afcc96236239fbfb43337fdca4dd8b793
Gerard Flanagan <Gerard.Flanagan@velocityww.com>
2015-05-03 19:29:18 +0100

    added a Settings proxy class

--- felicity/base.py
+++ felicity/base.py
@@ -1,6 +1,38 @@
 import sys
 
+from . import defaults
 from .utils import import_object
+
+DEFAULT_SETTINGS_MODULE = 'settings'
+
+class Settings(object):
+
+    def __init__(self):
+        self._defaults = defaults
+        settings = None
+        # try django settings
+        try:
+            from django.conf import settings
+        except ImportError:
+            # else look for settings.py in the current directory
+            try:
+                __import__(DEFAULT_SETTINGS_MODULE)
+            except ImportError:
+                pass
+            else:
+                settings = sys.modules[DEFAULT_SETTINGS_MODULE]
+        self._settings = settings
+
+    def __getattr__(self, key):
+        try:
+            result = getattr(self._settings, key)
+        except AttributeError:
+            try:
+                result = getattr(self._defaults, key)
+            except AttributeError:
+                raise AttributeError(key)
+        setattr(self, key, result)
+        return result
 
 class Config(object):
     """
@@ -8,24 +40,12 @@ class Config(object):
     """
 
     def __init__(self):
-        settings = None
-        # try django settings
-        try:
-            from django.conf import settings
-        except ImportError:
-            # look for settings.py in the current directory, otherwise fallback
-            # to defaults
-            for module in ['settings', 'felicity.defaults']:
-                try:
-                    __import__(module)
-                except ImportError:
-                    continue
-                else:
-                    settings = sys.modules[module]
-                    break
+        settings = Settings()
         try:
             cls = settings.FELICITY_BACKEND
         except AttributeError:
+            cls = None
+        if not cls:
             raise Exception('FELICITY_BACKEND is a required setting')
         cls = import_object(cls)
         backend = cls(settings)
@@ -50,5 +70,5 @@ class Config(object):
         self.backend.set(key, value)
 
     def __dir__(self):
-        return self.settings.FELICITY_CONFIG.keys() + ['settings', 'backend']
+        return self.settings.FELICITY_CONFIG.keys()
 

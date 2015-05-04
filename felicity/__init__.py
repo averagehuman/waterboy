259a5c9aa84bb6a0e22de75f685d42ea6cba167d
Gerard Flanagan <Gerard.Flanagan@velocityww.com>
2015-05-04 04:03:50 +0100

    Django-like "configure settings on first attribute access"

--- felicity/__init__.py
+++ felicity/__init__.py
@@ -1,14 +1,18 @@
 import lazy_object_proxy
-from .base import Config
+from .base import Config, Settings
 
 __version__ = '0.0.1'
 
-def LazyConfig():
-    return lazy_object_proxy.Proxy(Config)
+# non-editable settings
+def LazySettings():
+    return lazy_object_proxy.Proxy(Settings)
+
+settings = LazySettings()
 
 # editable, storage-backed settings
+def LazyConfig():
+    return lazy_object_proxy.Proxy(lambda: Config(settings))
+
 config = LazyConfig()
 
-# non-editable settings
-settings = lazy_object_proxy.Proxy(lambda: config.settings)
 
--- felicity/base.py
+++ felicity/base.py
@@ -1,36 +1,67 @@
 import sys
 
-from . import defaults
+import six
+
+from . import defaults as felicity_defaults
 from .utils import import_object
 
 DEFAULT_SETTINGS_MODULE = 'settings'
 
+EMPTY = object()
+
 class Settings(object):
 
     def __init__(self):
-        self._defaults = defaults
-        settings = None
-        # try django settings
-        try:
-            from django.conf import settings
-        except ImportError:
-            # else look for settings.py in the current directory
+        self.__dict__['_settings'] = EMPTY
+
+    def _update_settings_map(self, mapping, d):
+        """update mapping with values from d that have uppercase keys"""
+        for k, v in d.items():
+            if k and k == k.upper():
+                mapping[k] = v
+
+    def configure(self, settings_module=None, defaults=None):
+        d = {}
+        defaults = defaults or felicity_defaults.__dict__
+        self._update_settings_map(d, defaults)
+
+        overrrides = None
+        if isinstance(settings_module, six.string_types):
+            __import__(settings_module)
+            settings_module = sys.modules[settings_module]
+
+        if settings_module:
+            overrrides = settings_module.__dict__
+        else:
+            # try django settings
             try:
-                __import__(DEFAULT_SETTINGS_MODULE)
+                from django.conf import settings as django_settings
             except ImportError:
-                pass
+                # look for settings.py in the current directory
+                try:
+                    __import__(DEFAULT_SETTINGS_MODULE)
+                except ImportError:
+                    pass
+                else:
+                    overrrides = sys.modules[DEFAULT_SETTINGS_MODULE].__dict__
             else:
-                settings = sys.modules[DEFAULT_SETTINGS_MODULE]
-        self._settings = settings
+                # Django settings are proxied
+                overrrides = django_settings._wrapped.__dict__
+        if overrrides:
+            self._update_settings_map(d, overrrides)
+        self._settings = d
+
+    @property
+    def is_configured(self):
+        return self._settings is not EMPTY
 
     def __getattr__(self, key):
+        if self._settings is EMPTY:
+            self.configure()
         try:
-            result = getattr(self._settings, key)
-        except AttributeError:
-            try:
-                result = getattr(self._defaults, key)
-            except AttributeError:
-                raise AttributeError(key)
+            result = self._settings[key]
+        except KeyError:
+            raise AttributeError(key)
         setattr(self, key, result)
         return result
 
@@ -39,8 +70,8 @@ class Config(object):
     The global config wrapper that handles the backend.
     """
 
-    def __init__(self):
-        settings = Settings()
+    def __init__(self, settings=None):
+        settings = settings or Settings()
         try:
             cls = settings.FELICITY_BACKEND
         except AttributeError:
